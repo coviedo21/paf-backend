66c2e9afd37203423dd7c2b3713f50002a67a9f2
Alex Painen <alexpainen@gmail.com>
2025-02-10 17:01:36 -0300

    FiX: Cambios en la obtencion de licencias y finiquitos

--- solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/service/LicenciaFiniquitoService.java
+++ solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/service/LicenciaFiniquitoService.java
@@ -5,8 +5,9 @@ import cl.gob.ips.solicitudes_pago.dto.LicenciaFiniquitoInputDTO;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public interface LicenciaFiniquitoService {
-    List<LicenciaFiniquitoDTO> obtenerLicenciaFiniquito(int rutBeneficiario, String fechaInicio, String fechaFin);
+    List<LicenciaFiniquitoDTO> obtenerLicenciaFiniquito(int rutBeneficiario, String nroLicencia, String fechaInicio, String fechaFin);
     HashMap<String, String> agregarLicenciaFiniquito(LicenciaFiniquitoInputDTO licenciaFiniquito);
 }
