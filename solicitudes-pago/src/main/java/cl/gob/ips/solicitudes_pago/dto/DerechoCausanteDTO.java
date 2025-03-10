ed99284a67d815c6b351f5bc1f436bf66fad6470
coviedo21 <coviedog.srv@ips.gob.cl>
2025-03-10 17:46:58 -0300

    Cambio tipo de dato Long porque ID del derecho causante es muy grande

--- solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/dto/DerechoCausanteDTO.java
+++ solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/dto/DerechoCausanteDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class DerechoCausanteDTO {
-    private int id;
+    private Long id;
     private int rutCausante;
     private String dvCausante;
     private int rutBeneficiario;
