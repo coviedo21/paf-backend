8e7e4cc64a9700a1131bab74b8cfe56cf4e970c8
coviedo21 <coviedog.srv@ips.gob.cl>
2025-03-11 17:01:52 -0300

    Se agrega dvEmpleador como resultado de la API de cuenta corriente

--- solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/dto/DerechoCausanteDTO.java
+++ solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/dto/DerechoCausanteDTO.java
@@ -20,6 +20,7 @@ public class DerechoCausanteDTO {
     private int codigoTramo;
     private int diasReconocimiento;
     private int rutEmpleador;
+    private int dvEmpleador;
     private int cantidadPagos;
     private int montoPagos;
     private int diferencia;
