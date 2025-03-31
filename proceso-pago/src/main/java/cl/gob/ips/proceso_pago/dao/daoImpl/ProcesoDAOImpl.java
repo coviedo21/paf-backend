085773570675a608b3c1e37cd7f0f85dd59faeeb
coviedo21 <coviedog.srv@ips.gob.cl>
2025-03-31 19:21:53 -0300

    Cambios iNis por rutNis

--- proceso-pago/src/main/java/cl/gob/ips/proceso_pago/dao/daoImpl/ProcesoDAOImpl.java
+++ proceso-pago/src/main/java/cl/gob/ips/proceso_pago/dao/daoImpl/ProcesoDAOImpl.java
@@ -400,7 +400,7 @@ public void insertarProcesoTipoSolicitante(int idProceso, List<Integer> listaTip
                     detalleDTO.setDvEmpleador((String) row.get("dvEmpleador"));
 
                 if (row.get("nis") != null)
-                    detalleDTO.setINis((String) row.get("nis"));
+                    detalleDTO.setRutNis((String) row.get("nis"));
 
                 if (row.get("dvNis") != null)
                     detalleDTO.setDvNis((String) row.get("dvNis"));
--- proceso-pago/src/main/java/cl/gob/ips/proceso_pago/dto/DetalleCausanteDTO.java
+++ proceso-pago/src/main/java/cl/gob/ips/proceso_pago/dto/DetalleCausanteDTO.java
@@ -30,7 +30,7 @@ public class DetalleCausanteDTO {
     private String nombreEstado;
     private int rutEmpleador;
     private String dvEmpleador;
-    private String iNis;
+    private String rutNis;
     private String dvNis;
     private int numeroDocumento;
     private String dvDocumento;
