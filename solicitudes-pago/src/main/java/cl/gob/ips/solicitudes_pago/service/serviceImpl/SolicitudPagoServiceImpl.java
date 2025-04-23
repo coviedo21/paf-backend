af81e466e0a6fcd15469b3c3322e2fa746c192b6
coviedo21 <coviedog.srv@ips.gob.cl>
2025-04-23 13:12:27 -0400

    Que no envie correo en un caso especifico

--- solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/service/serviceImpl/SolicitudPagoServiceImpl.java
+++ solicitudes-pago/src/main/java/cl/gob/ips/solicitudes_pago/service/serviceImpl/SolicitudPagoServiceImpl.java
@@ -80,7 +80,9 @@ public class SolicitudPagoServiceImpl implements SolicitudPagoService {
                     actualizarSolicitud.setCumpleCriterios("N");
                     actualizarSolicitudPago(actualizarSolicitud);
                     response.setGlosaRetorno("Solicitud Nº "+(int) response.getResultado()+" creada. No cumple con todos los criterios de aceptación.");
-                    emailService.enviarCorreo(solicitudPago.getEmail(),"Solicitud N°"+(int) response.getResultado()+" recibida con error(es)","Su solicitud N° "+(int) response.getResultado()+" NO cumple con todos los criterios de aceptación. A partir de este momento cuenta con "+diasAntiguedad+" días para subsanarla. Deberá adjuntar la documentación necesaria, de lo contrario será enviada automáticamente para su resolución");    
+                    if(!esArchivo){
+                    	emailService.enviarCorreo(solicitudPago.getEmail(),"Solicitud N°"+(int) response.getResultado()+" recibida con error(es)","Su solicitud N° "+(int) response.getResultado()+" NO cumple con todos los criterios de aceptación. A partir de este momento cuenta con "+diasAntiguedad+" días para subsanarla. Deberá adjuntar la documentación necesaria, de lo contrario será enviada automáticamente para su resolución");
+                    }
                 } catch (Exception e) {
                     // Captura cualquier excepción relacionada con el envío del correo y loguea el error
                     System.err.println("Error enviando correo para la solicitud " + (int) response.getResultado() + ": " + e.getMessage());
